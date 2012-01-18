323177c2ac4959d283417ef2e0b7d32eeeca01ee
druidvav <druidvav@gmail.com>
2012-01-18 06:03:40 +0400

    Изменение версии перед бранчингом

--- nbackup.php
+++ nbackup.php
@@ -2,5 +2,8 @@
 include_once('./lib/Backup.php');
 include_once('./lib/yaml.php');
 
+echo "nekoBackup 1.0beta1\n";
+echo "\n";
+
 $backup = new Backup(Spyc::YAMLLoad('./cfg/config.yaml'));
 $backup->execute(@$_SERVER['argv'][1] == 'initial' ? 'initial' : time());
