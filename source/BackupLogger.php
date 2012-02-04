49e1f53b1b4be6ae6991cd58ec51c20885c2708d
druidvav <druidvav@gmail.com>
2012-02-04 15:59:26 +0400

    Fixes #3: Automatic logging to file

--- source/BackupLogger.php
+++ source/BackupLogger.php
@@ -8,8 +8,28 @@ class BackupLogger
   {
     $date = date("Y.m.d H:i:s");
     $indent = self::$log_indent ? implode(' > ', self::$log_indent) . ' > ' : '';
-    echo "$date > $level > {$indent}$message \n";
+    $message = "$date > $level > {$indent}$message \n";
+
+    echo $message;
+    self::appendFile($message);
   }
+
+  public static function appendFile($message)
+  {
+    $fh = fopen(LOG_PATH, 'a+') or die('Couldn\'t open logfile');
+    if(flock($fh, LOCK_EX))
+    {
+      fseek($fh, 0, SEEK_END);
+      fwrite($fh, $message);
+    }
+    else
+    {
+      die('Couldn\'t block logfile for exclusive access');
+    }
+    fclose($fh);
+    @chmod(LOG_PATH, 0666);
+  }
+
 
   public static function indent($group) { self::$log_indent[] = $group; }
   public static function back() { array_pop(self::$log_indent); }
