d7cabc7d7edb730b7cbebf2eabc1a5fe0e8a4136
druidvav <druidvav@gmail.com>
2013-06-09 01:02:13 +0400

    symfony/console integrated

--- nbackup.php
+++ nbackup.php
@@ -6,24 +6,39 @@ define('LOG_PATH',    '/var/log/nbackup.log');
 
 include_once(VENDOR_PATH . 'autoload.php');
 
-echo "  >>  nekoBackup 1.3alpha by druidvav  << \n";
-echo "\n";
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-$opts = getopt('', array('driver:', 'initial', 'install'));
+$console = new Application();
+$console->setName('[ nekoBackup by druidvav ]');
+$console->setVersion('1.3rc');
+$console->register('backup')
+  ->setDefinition(array(
+    new InputArgument('driver', InputArgument::OPTIONAL, 'Backup driver [basic, s3]', 'basic'),
+    new InputOption('initial', 'i', InputOption::VALUE_OPTIONAL, 'Run the first backup with this parameter set to 1', 0),
+  ))
+  ->setDescription('Starts backup process')
+  ->setCode(function (InputInterface $input, OutputInterface $output) {
+    $app = new nekoBackup\App($input->getArgument('driver'));
+    if($input->getOption('initial')) {
+      $app->setIsInitial(true);
+    }
+    $app->bootstrap();
+  });
+$console->register('install')
+  ->setDefinition(array(
+    new InputArgument('driver', InputArgument::OPTIONAL, 'Backup driver [basic, s3]', 'basic'),
+  ))
+  ->setDescription('Install backup script to crontab')
+  ->setCode(function (InputInterface $input, OutputInterface $output) {
+    $driver = $input->getArgument('driver');
 
-if(isset($opts['install']))
-{
-  echo "Installing crontab...";
-
-  $line = "0 3 * * * php " . __FILE__ . " --driver={$opts['driver']} &> /dev/null\n";
-  `(crontab -l; echo "{$line}") | crontab -`;
-
-  echo " done.\n";
-  exit;
-}
-
-$app = new \nekoBackup\App(@$opts['driver']);
-if(!empty($opts['initial'])) {
-  $app->setIsInitial(true);
-}
-$app->bootstrap();
+    $output->write("Installing crontab...");
+    $line = "30 1 * * * php " . __FILE__ . " backup {$driver} &> /dev/null\n";
+    exec("(crontab -l; echo \"{$line}\") | crontab -");
+    $output->writeln(" done.");
+  });
+$console->run();
