77fd6a84c13acd6c8c854f4040392de0571bfc73
druidvav <druidvav@gmail.com>
2013-06-09 01:20:32 +0400

    Небольшой рефакторинг и обновление ридми

--- nbackup.php
+++ nbackup.php
@@ -1,44 +1,10 @@
 <?php
+define('VERSION', '1.3rc1');
 define('CONFIG_PATH', dirname(__FILE__) . '/config/');
 define('SOURCE_PATH', dirname(__FILE__) . '/source/');
 define('VENDOR_PATH', dirname(__FILE__) . '/vendor/');
 define('LOG_PATH',    '/var/log/nbackup.log');
 
 include_once(VENDOR_PATH . 'autoload.php');
+include_once(SOURCE_PATH . 'Console.php');
 
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-
-$console = new Application();
-$console->setName('[ nekoBackup by druidvav ]');
-$console->setVersion('1.3rc');
-$console->register('backup')
-  ->setDefinition(array(
-    new InputArgument('driver', InputArgument::OPTIONAL, 'Backup driver [basic, s3]', 'basic'),
-    new InputOption('initial', 'i', InputOption::VALUE_OPTIONAL, 'Run the first backup with this parameter set to 1', 0),
-  ))
-  ->setDescription('Starts backup process')
-  ->setCode(function (InputInterface $input, OutputInterface $output) {
-    $app = new nekoBackup\App($input->getArgument('driver'));
-    if($input->getOption('initial')) {
-      $app->setIsInitial(true);
-    }
-    $app->bootstrap();
-  });
-$console->register('install')
-  ->setDefinition(array(
-    new InputArgument('driver', InputArgument::OPTIONAL, 'Backup driver [basic, s3]', 'basic'),
-  ))
-  ->setDescription('Install backup script to crontab')
-  ->setCode(function (InputInterface $input, OutputInterface $output) {
-    $driver = $input->getArgument('driver');
-
-    $output->write("Installing crontab...");
-    $line = "30 1 * * * php " . __FILE__ . " backup {$driver} &> /dev/null\n";
-    exec("(crontab -l; echo \"{$line}\") | crontab -");
-    $output->writeln(" done.");
-  });
-$console->run();
---
+++ source/Console.php
@@ -0,0 +1,37 @@
+<?php
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+$console = new Application();
+$console->setName('[ nekoBackup by druidvav ]');
+$console->setVersion(VERSION);
+$console->register('backup')
+  ->setDefinition(array(
+    new InputArgument('driver', InputArgument::OPTIONAL, 'Backup driver [basic, s3]', 'basic'),
+    new InputOption('initial', 'i', InputOption::VALUE_OPTIONAL, 'Run the first backup with this parameter set to 1', 0),
+  ))
+  ->setDescription('Start backup process')
+  ->setCode(function (InputInterface $input, OutputInterface $output) {
+    $app = new nekoBackup\App($input->getArgument('driver'));
+    if($input->getOption('initial')) {
+      $app->setIsInitial(true);
+    }
+    $app->bootstrap();
+  });
+$console->register('install')
+  ->setDefinition(array(
+    new InputArgument('driver', InputArgument::OPTIONAL, 'Backup driver [basic, s3]', 'basic'),
+  ))
+  ->setDescription('Install backup script to crontab')
+  ->setCode(function (InputInterface $input, OutputInterface $output) {
+    $driver = $input->getArgument('driver');
+
+    $output->write("Installing crontab...");
+    $line = "30 1 * * * php " . __FILE__ . " backup {$driver} &> /dev/null\n";
+    exec("(crontab -l; echo \"{$line}\") | crontab -");
+    $output->writeln(" done.");
+  });
+$console->run();
